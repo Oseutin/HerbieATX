3fb7445db30df780c523e1bb5ea5b5cf8576ad18
Oseutin <oseutin@naver.com>
2023-07-19 12:17:45 +0200

    Update speech_sample.py

--- speech_sample.py
+++ speech_sample.py
@@ -28,7 +28,7 @@ except ImportError:
 
 # Set up the subscription info for the Speech Service:
 # Replace with your own subscription key and service region (e.g., "westus").
-speech_key, service_region = "YourSubscriptionKey", "YourServiceRegion"
+speech_key, service_region = "dc48a3b483f1489494e30955ff2c5481", "westeurope"
 
 # Specify the path to an audio file containing speech (mono WAV / PCM with a sampling rate of 16
 # kHz).
