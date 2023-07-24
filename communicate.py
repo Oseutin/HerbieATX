50c312519cf8f4005dfd720d4331952c5e8018d1
Oseutin <oseutin@naver.com>
2023-07-24 12:17:11 +0200

    Update communicate.py

--- communicate.py
+++ communicate.py
@@ -5,7 +5,7 @@
 
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 # sets up the OpenAI API client
 model_engine = "gpt-3.5-turbo"
