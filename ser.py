d7d05aa1d6a9be7d7804e31a542f183228b868fb
Oseutin <oseutin@naver.com>
2023-07-31 11:07:26 +0200

    Quick Serial Fix

--- ser.py
+++ ser.py
@@ -2,7 +2,7 @@ import serial
 with serial.Serial('/dev/ttyACM0', 9600, timeout=10) as ser:  # open serial port
     while True:
         light_data = open("lightSense.txt", "w")
-        decoded_string = ser.readLine().decode("utf-8")
+        decoded_string = ser.readline().decode("utf-8")
         light_data.write(decoded_string)
         light_data.close()
         # print(ser.readline())
